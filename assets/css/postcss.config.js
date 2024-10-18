26d0a09952f39bb4a718a04aa6390c139ff4f586
Horikita_Saku <saku2696983188@gmail.com>
2024-10-18 15:59:28 +0800

    update

--- assets/css/postcss.config.js
+++ assets/css/postcss.config.js
@@ -3,14 +3,6 @@ const themeDir = __dirname + "/../../";
 module.exports = {
   plugins: [
     require("tailwindcss")(themeDir + "assets/css/tailwind.config.js"),
-    require("postcss-import")(),
-    require("postcss-nested")(),
-    require("postcss-preset-env")({
-      stage: 1,
-      features: {
-        'nesting-rules': true,
-      },
-    }),
     require("autoprefixer")({
       path: [themeDir],
     }),
